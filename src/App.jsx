92c10a33df3c3339b9770e501cac4e4667e81653
chatchai <chsuriyawan@yahoo.com>
2026-02-01 20:46:51 +0700

    starter with react-tailwincss-daisyui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="app">
+     <h1 className="text-3xl text-amber-300">Hello, Papoo!</h1>
+     <button className="btn btn-primary">Click Me</button>
+    </div>
+  )
+}
+
+export default App
