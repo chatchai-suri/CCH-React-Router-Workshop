b1870d078eb4b2667c3b48a3966058d6bd9066bf
chatchai <chsuriyawan@yahoo.com>
2026-02-14 19:31:53 +0700

    add language change button at Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,30 @@
-import { NavLink, useParams } from "react-router";
+import { NavLink, useLocation, useNavigate, useParams } from "react-router";
 
 function Navbar() {
   const {lang} = useParams()
+  const {pathname} = useLocation()
+  const navigate = useNavigate()
   const style = {
-    liLink: "hover:font-medium hover:text-[#826047]"
+    liLink: "hover:font-medium hover:text-[#826047]",
+    langBtn: "px-2 py-1 cursor-pointer hover:font-bold border rounded-md text-[10px]"
+  }
+
+  // function for toggle language
+  const changeLanguage = (newLang) => {
+    // pathname e.g. /en/lifestyle --> keep only /lifestyle
+    const currentPathname =  pathname.split("/").slice(2).join("/")
+    console.log(currentPathname)
+    // e.g. redirect to /th/lifestyle
+    navigate(`/${newLang}/${currentPathname}`)
   }
   return (
-    <nav className="py-4">
+    <nav className="py-4 flex flex-col items-center gap-2">
+      <div className="flex gap-2 mb-2">
+        <button onClick={()=>changeLanguage("th")} className={`${style.langBtn} ${lang==="th" ? "bg-amber-200" : "bg-white"}`} >TH</button>
+        <button onClick={()=>changeLanguage("en")} className={`${style.langBtn} ${lang==="en" ? "bg-amber-200" : "bg-white"}`} >EN</button>
+      </div>
       <ul className="flex gap-1 text-xs font-normal">
-        <li className={style.liLink}><NavLink to="/">HOME /</NavLink></li>
+        <li className={style.liLink}><NavLink to={`/${lang}`}>HOME /</NavLink></li>
         <li className={style.liLink}><NavLink to={`/${lang}/lifestyle`}>LIFESTYLE /</NavLink></li>
         <li className={style.liLink}><NavLink to={`/${lang}/journey`}>JOURNEY /</NavLink></li>
         <li className={style.liLink}><NavLink to={`/${lang}/inspiration`}>INSPIRATION /</NavLink></li>
--- src/components/shop/ShopLayout.jsx
+++ src/components/shop/ShopLayout.jsx
@@ -7,7 +7,7 @@ function ShopLayout() {
     <Card>
       <h1>หมวดหมู่สินค้า</h1>
       <div>
-        <NavLink to={`/${lang}/shop/electronis`}>Electronics | </NavLink>
+        <NavLink to={`/${lang}/shop/electroncis`}>Electronics | </NavLink>
         <NavLink to={`/${lang}/shop/clothing`}>Clothing | </NavLink>
         <NavLink to={`/${lang}/shop/furniture`}>Furniture</NavLink>
       </div>
